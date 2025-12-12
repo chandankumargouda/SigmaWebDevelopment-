14e9f5e7c09da7978bc23d60484bfef4753f8e38
Chandan Kumar Gouda <chandankumargouda514@gmail.com>
2025-12-12 23:22:50 +0530

    This is my first clone

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+// alert("Hello! My name is Chandan");
+
+console.log("Code is Running.....");
+console.log("Code s also running.....");
+console.log("Code is loking like wow");
+
+// var a=prompt("Enter a Number")
+// var isTrue=confirm("Are you sure to proceed")
+// if(isTrue){
+//     console.log("Laptop is Blasting")
+// }else{
+//     console.log("you are safe now")
+// }
+console.log("Your number is :" + a)
